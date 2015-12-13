670d8c4757aadfa5287cb4c51a29a742f27010b1
Jimmy Cao <jc2462@cornell.edu>
2015-12-13 01:47:17 -0500

    Crawling works for the republican nominees national and in iowa.

--- scraper.py
+++ scraper.py
@@ -1,20 +1,22 @@
-import sys, os
-args = sys.argv
+import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="URL of the page with the polling data")
+parser.add_argument("outputfile", nargs='?', help="CSV output file")
+parser.add_argument("--noavg", help="Do not include the RCP Average row",
+                    action="store_true")
+args = parser.parse_args()
 
-if (len(args) <= 1):
-    print("ERROR: Please provide source url")
-    print("Example : python scraper.py url output.csv")
-else:
-    url = args[1]
-    if (len(args) == 2):
-        filename = url.split('/')[-1].split('.')[0]
-        output = filename + ".csv"
-        print("No output file specified : using " + output)
-    else:
-        output = sys.argv[2]
-        if not output.endswith(".csv"):
-            output = output + ".csv"
-    if os.path.isfile(output):
-        os.remove(output)
-    os.system("scrapy crawl realclearpoliticsSpider -a url="+url+" -o "+output)
+url = args.url
+output = args.outputfile
+if output is None:
+    filename = url.split('/')[-1].split('.')[0]
+    output = filename + ".csv"
+    print("No output file specified : using " + output)
+elif not output.endswith(".csv"):
+    output += ".csv"
+if os.path.isfile(output):
+    os.remove(output)
+noavg_opt = " -a noavg=1" if args.noavg else ""
+os.system("scrapy crawl realclearpoliticsSpider -a url="+url+noavg_opt+" -o "+output)
 
